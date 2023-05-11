eedb6fde296269353dd34e1ad65699b709a54c85
Justin Stitt <jstitt007@gmail.com>
2023-05-10 18:19:10 -0700

    server now object

--- nc-poker/server.py
+++ nc-poker/server.py
@@ -11,75 +11,74 @@ HOST = os.getenv("HOST", "0.0.0.0")
 PORT = int(os.getenv("PORT", "1234"))
 DRAW: dict[str, bytes] = {"clear": b"\x1b[2J\x1b[H"}
 
-clients: dict[Address, socket.socket] = {}
 
+class Server:
+    def __init__(self, *, host: str, port: int):
+        self.clients: dict[Address, socket.socket] = {}
+        self.server = self.setup_server(host=host, port=port)
 
-def process_data(data) -> None:
-    print(f"{data=}")
+    def setup_server(self, *, host: str, port: int) -> socket.socket:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.bind((host, port))
+        server.listen()
 
+        # TODO: replace with logging
+        print(f"Started server on: {host=}, {port=}. Awaiting Connections...")
+        return server
 
-def handle_client(conn: socket.socket, addr: Address) -> None:
-    clients[addr] = conn
-    print(f"✔ CONNECTED: {conn=}, {addr=}")
+    def broadcast(self, message: bytes) -> None:
+        for _, conn in self.clients.items():
+            conn.sendall(message)
 
-    welcome_message = (
-        f"b'Welcome to Netcat Poker. Lobby #1 ({len(clients.keys())} / 6)'"
-    )
-    # send a welcome message to the client
-    conn.sendall(DRAW["clear"])
-    conn.sendall(
-        b"Welcome to the poker game!" + bytes(str(addr), encoding="utf-8") + b"\n"
-    )
+    def handle_client_connection(self, conn: socket.socket, addr: Address) -> None:
+        self.clients[addr] = conn
+        print(f"✔ CONNECTED: {conn=}, {addr=}")
 
-    # send and receive data with the client on this thread
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        conn.sendall(b"You said: " + data + b"\n")
-        # response = process_data(data)
+        welcome_message = f"Welcome to Netcat Poker. Lobby #1 ({len(self.clients.keys())} / 6)\nWaiting for game to start..."
 
-    print(f"❌ DISCONNECTED {clients[addr]}")
-    del clients[addr]
-    conn.close()
+        # send welcome message to all self.clients (including freshly connected one)
+        self.broadcast(message=DRAW["clear"])
+        self.broadcast(message=welcome_message.encode())
 
+        # send and receive data with the client on this thread
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(b"You said: " + data + b"\n")
 
-def setup_server(*, host: str, port: int) -> socket.socket:
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind((host, port))
-    server.listen()
+        print(f"❌ DISCONNECTED {self.clients[addr]}")
+        del self.clients[addr]
+        conn.close()
 
-    print(f"Started server on: {host=}, {port=}. Awaiting Connections...")
+    def kill_server_and_connections(self) -> None:
+        print("☠ Killing Server... ", self)
+        print("🤼 Disconnecting Clients" if len(self.clients.keys()) else "")
+        for _, client_socket in self.clients.items():
+            client_socket.shutdown(socket.SHUT_RDWR)
+            client_socket.close()
 
-    return server
+        self.server.shutdown(socket.SHUT_RDWR)  # gracefully kill server
+        self.server.close()
 
-
-def kill_server_and_connections(server: socket.socket) -> None:
-    print("☠ Killing Server...")
-    print("🤼 Disconnecting Clients" if len(clients.keys()) else "")
-    for _, client_socket in clients.items():
-        client_socket.shutdown(socket.SHUT_RDWR)
-        client_socket.close()
-    server.shutdown(socket.SHUT_RDWR)  # gracefully kill server
-    server.close()
-
-
-def serve_forever(server: socket.socket) -> None:
-    while True:
-        try:
-            conn, addr = server.accept()
-            # start a new thread to handle the client connection
-            t = threading.Thread(target=handle_client, args=(conn, addr))
-            t.start()
-        except KeyboardInterrupt:
-            kill_server_and_connections(server)
-            exit(0)  # gracefully kill interpreter
+    def serve_forever(self) -> None:
+        while True:
+            try:
+                conn, addr = self.server.accept()
+                # start a new thread to handle the client connection
+                t = threading.Thread(
+                    target=self.handle_client_connection, args=(conn, addr)
+                )
+                t.start()
+            except KeyboardInterrupt:
+                self.kill_server_and_connections()
+                exit(0)  # gracefully kill interpreter
 
 
 def main():
-    server = setup_server(host=HOST, port=PORT)
-    serve_forever(server)
+    server = Server(host=HOST, port=PORT)
+    server.serve_forever()
 
 
 if __name__ == "__main__":
